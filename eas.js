debfc14216478beb5f31a27b73831ec65bf1d254
Chris <cpitaz108@gmail.com>
2024-11-12 22:33:44 -0800

    Got the initial sizing for rows correct, will add more to adjust the sizes dynamically to keep the aspect ratio square

--- eas.js
+++ eas.js
@@ -1,1 +1,24 @@
-document.getElementById("eas-screen")
+function createRectangles(resolution){
+  screen = document.getElementById("eas-screen");
+  for(let i = 0; i < resolution; i++){
+    let row = populateRow(resolution, screen);
+    screen.appendChild(row);
+  }
+}
+
+function populateRow(resolution, screen){
+  let row = document.createElement("div");
+  row.style.width = "100%";
+  row.style.height = (screen.clientHeight / resolution).toString() + "px";
+  row.style.boxSizing = "border-box";
+  row.style.border = "1px solid black";
+  row.classList.add("screen-row");
+  for(let i = 0; i < resolution; i++){
+      let box = document.createElement("div");
+      box.classList.add("screen-box");
+      box.width;
+      row.appendChild(box);
+  }
+  return row;
+}
+
